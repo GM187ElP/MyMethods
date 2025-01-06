a0d49bfe82979b62853fe84b16e663c88da1f755
PGM <arsalan.akorn@gmail.com>
2025-01-06 23:02:20 +0330

    Update RandomDataMaker.cs

--- RandomDataMaker.cs
+++ RandomDataMaker.cs
@@ -8,7 +8,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ML2;
+namespace YourNamespace;
 
 public class RandomDataMaker<T> where T : class, new()
 {
